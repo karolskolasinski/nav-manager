f072214f05d89fea2862a9ed778623b692a1b122
Karol Skolasiński <karol.skolasinski@7willows.com>
2024-12-02 09:21:50 +0100

    feat: add contracts

---
+++ src/app/contrancts.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+const Item = z.object({
+  label: z.string(),
+  url: z.string().url(),
+});
+
+type Items = z.infer<typeof Item> & {
+  subitems: Items[];
+};
+
+export const NavItem: z.ZodType<Items> = Item.extend({
+  subitems: z.lazy(() => NavItem.array()),
+});
+export type NavItem = z.infer<typeof NavItem>;

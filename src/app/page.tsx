3372b9350a9a1919fc573c1123877c367adc14e6
Karol Skolasiński <karol.skolasinski@7willows.com>
2024-12-02 12:06:31 +0100

    feat: render nav item list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import { NavItem } from "@/app/contrancts";
 
 export default function Home() {
-  return <div>HELLO</div>;
+  const [isFormVisible, setIsFormVisible] = useState(false);
+  const [items, setItems] = useState([] as NavItem[]);
+
+  const handleShowFormClick = () => setIsFormVisible(true);
+
+  const addItem = (data: NavItem) => {
+    const newItems: NavItem[] = [...items, data];
+    setItems(newItems);
+    setIsFormVisible(false);
+  };
+
+  return (
+    <>
+      {items.map((item) => <div>item</div>)}
+    </>
+  );
 }
